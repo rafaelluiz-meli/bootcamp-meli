0c909a9a5b29f6303ae92a0c39023c0710c47e4b
Rafael Santana <rafael.santana@mercadolivre.com>
2022-03-17 17:58:21 -0300

    Correcao dos nomes na classe e controller

--- spring/integradora-1/src/main/java/com/mercadolivre/integradora1/controller/ConvertNumberController.java
+++ spring/integradora-1/src/main/java/com/mercadolivre/integradora1/controller/ConvertNumberController.java
@@ -11,11 +11,11 @@ public class ConvertNumberController {
 
     @GetMapping("/convert")
     @ResponseBody
-    public String returnRomanNumber(@RequestParam Integer decimal){
+    public String returnRomanNumber(@RequestParam Integer number){
         ConvertNumber convertNumber = new ConvertNumber();
-        String convert = convertNumber.decimalToRoman(decimal);
+        String convert = convertNumber.integerToRoman(number);
         StringBuilder result = new StringBuilder();
-        result.append("Decimal: " + decimal);
+        result.append("Decimal: " + number);
         result.append("Roman: " + convert);
         return result.toString();
     }
--- spring/integradora-1/src/main/java/com/mercadolivre/integradora1/model/ConvertNumber.java
+++ spring/integradora-1/src/main/java/com/mercadolivre/integradora1/model/ConvertNumber.java
@@ -2,16 +2,16 @@ package com.mercadolivre.integradora1.model;
 
 public class ConvertNumber {
 
-    public String decimalToRoman(Integer decimal){
+    public String integerToRoman(Integer number){
 
-        int[] decimalRef = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        int[] integerRef = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
         String[] romanRef = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
 
         StringBuilder roman = new StringBuilder();
 
-        for (int i = 0; i<decimalRef.length;i++){
-            while(decimal >= decimalRef[i]){
-                decimal -= decimalRef[i];
+        for (int i = 0; i<integerRef.length;i++){
+            while(number >= integerRef[i]){
+                number -= integerRef[i];
                 roman.append(romanRef[i]);
             }
         }
