55c01444335b919591430ada3faa39c54ed92a2b
Rafael Santana <rafael.santana@mercadolivre.com>
2022-04-19 20:46:07 -0300

    Initial Project"

---
+++ storage/hibernate-with-spring/src/main/java/com/mercadolivre/hibernatewithspring/HibernateWithSpringApplication.java
@@ -0,0 +1,13 @@
+package com.mercadolivre.hibernatewithspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateWithSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibernateWithSpringApplication.class, args);
+    }
+
+}
