647ba0fecf8e66b665f742201f975a72577e0047
Rafael Santana <rafael.santana@mercadolivre.com>
2022-04-19 22:19:47 -0300

    Initial Project

---
+++ storage/integradora-1/src/main/java/com/mercadolivre/integradora1/Integradora1Application.java
@@ -0,0 +1,13 @@
+package com.mercadolivre.integradora1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Integradora1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Integradora1Application.class, args);
+	}
+
+}
